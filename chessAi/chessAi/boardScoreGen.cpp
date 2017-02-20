cd0697dd752621cd509f80ce3325e149bd88dee1
ajpowell021 <ajpowell021@gmail.com>
2017-02-19 22:08:58 -0500

    Create pawn score generator.

---
+++ chessAi/chessAi/boardScoreGen.cpp
@@ -0,0 +1,45 @@
+// boardScoreGen.cpp calculates the score of an entire board by calling the different
+// piece specific functions from different files and adding them all together.
+
+#include <iostream>
+
+using namespace std;
+
+// Piece Functions
+int generatePawnScore(int location, int color);
+
+// Board Functions
+int getPieceType(int location);
+
+// Calculates the board score,
+// color = 0 if for white,
+// color = 1 if for black.
+int calcBoardScore(int color){
+
+    int boardScore = 0;
+    int colorModifier;
+
+    if(color == 0){
+        colorModifier = 1;
+    }
+    else{
+        colorModifier = -1;
+    }
+
+    for(int i = 0; i < 64; i++){
+
+        int tempPiece = getPieceType(i);
+        if(tempPiece == 1 || tempPiece == -1){
+            switch (tempPiece){
+                case -1:
+                    boardScore += (colorModifier * generatePawnScore(i, color));
+                    break;
+                case 1:
+                    boardScore += (colorModifier * generatePawnScore(i, color));
+                    break;
+            }
+        }
+    }
+
+    return boardScore;
+}
