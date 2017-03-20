5a33fd4e2fc1fcd613ad197aa246ec0743b3bfcc
ajpowell021 <ajpowell021@gmail.com>
2017-03-19 21:00:32 -0400

    Begin to iterate through all possible moves.

--- chessAi/chessAi/artificialIntelligence.cpp
+++ chessAi/chessAi/artificialIntelligence.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +11,13 @@ void beginTurn(int color);
 void turnChecker(int location);
 void addToArray(int move);
 void addCastleToMoves(int pieceNumber);
+int getPieceType();
+int getCapturedPiece();
+int getOrigin();
+int getDestination();
+void makeMove(int location, int arrayPos);
+int findSizeOfMoveArray();
+void displayScoreArray();
 
 // Other functions that are needed.
 int getPieceType(int pieceLocation);
@@ -20,6 +29,7 @@ bool getTopRightCastle();
 bool getBotLeftCastle();
 bool getBotRightCastle();
 int *getLegalMoves(int pieceType, int pieceLocation);
+int calcBoardScore(int color);
 
 // Storing current board position.
 int origin;
@@ -31,6 +41,8 @@ int capturedPiece;
 // Array holding moves for a piece.
 int movesForPiece[40];
 int arraySpot = 0;
+int scoreArray[10000];
+int scoreArraySpot = 0;
 
 void storeBoardInfo(int from, int to) {
     pieceType = getPieceType(from);
@@ -39,11 +51,28 @@ void storeBoardInfo(int from, int to) {
     destination = to;
 }
 
+int getPieceType() {
+    return pieceType;
+}
+
+int getCapturedPiece() {
+    return capturedPiece;
+}
+
+int getOrigin() {
+    return origin;
+}
+
+int getDestination() {
+    return destination;
+}
+
 // Main contacts the ai, letting it
 // know it is it's turn.
 // AI finds a piece.
 void beginTurn(int color) {
 
+    fill_n(scoreArray, 10000, -999999);
     // First, we search for a piece of the same color.
     for (int i = 0; i < 64; i++) {
         if(color == 0){
@@ -84,11 +113,76 @@ void turnChecker(int location) {
     // still concerned with moving the rook on a castle? Need to handle that
     // logic here.
 
+    // Carry out move.
+    int arraySize = findSizeOfMoveArray();
+
+    for (int i = 0; i < arraySize; i++) {
+        cout << i << endl;
+        makeMove(location, i);
+    }
+}
+
+// Makes the move on the board, returns the score
+// and then resets the board.
+void makeMove(int location, int arrayPos) {
+    int score = 0;
+    storeBoardInfo(location, movesForPiece[arrayPos]);
+
+    if(getCapturedPiece() != 0){
+        removePiece(getDestination());
+    }
+
+    removePiece(getOrigin());
+    addPiece(getPieceType(), getDestination());
+
+    if(getPieceType() > 0){
+        score = calcBoardScore(0);
+    }
+    else{
+        score = calcBoardScore(1);
+    }
+
+    scoreArray[scoreArraySpot] = score;
+    scoreArraySpot++;
+
+    //SORTT THIS ALL OUT
+
+    removePiece(getDestination());
+    addPiece(getCapturedPiece(), getDestination());
+    addPiece(getPieceType(), getOrigin());
+    //cout << "1: " << scoreArray[0] << endl;
+    //cout << "2: " << scoreArray[1] << endl;
+    //cout << "3: " << scoreArray[2] << endl;
+    //displayScoreArray();
+}
+
+void displayScoreArray(){
+
+    cout << "START OF PIECE" << endl;
+
+    for (int i = 0; i < scoreArraySpot + 1; i++) {
+        if(scoreArray[i] != -999999){
+            cout << scoreArray[i] << endl;
+        }
+    }
+    cout << "END OF PIECE" << endl;
 }
 
 void addToArray(int move) {
     movesForPiece[arraySpot] = move;
     arraySpot++;
+}
+
+int findSizeOfMoveArray(){
+    int counter = 0;
+
+    for (int i = 0; i < 41; i++) {
+        if (movesForPiece[i] > -1) {
+            counter++;
+        }
+    }
+
+    return counter;
 }
 
 // If a castle is possible, add it to array here.
